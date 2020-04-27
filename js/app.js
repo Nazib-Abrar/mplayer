5a95d27ef7d6e6d341a4779327e1dcb92ffbf070
isfatul-codersbd <39835738+isfatul-codersbd@users.noreply.github.com>
2020-04-27 16:59:25 +0600

    Cut the window.click func

--- js/app.js
+++ js/app.js
@@ -43,15 +43,7 @@ $(document).ready(function ()
 
 
 
-// Get the modal
-var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 //alert("linked");
 
 
